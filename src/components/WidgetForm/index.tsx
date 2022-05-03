f8955874adebaaea2c43a564b3aaaf0dd81001d3
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-05-03 18:23:59 -0300

    second

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,15 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
 
-import bugImageUrl from "../assets/bug.svg"
-import ideaImageUrl from "../assets/idea.svg"
-import otherImageUrl from "../assets/other.svg"
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg"
+import ideaImageUrl from "../../assets/idea.svg"
+import otherImageUrl from "../../assets/other.svg"
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -29,24 +33,23 @@ const feedbackTypes = {
     }
 }
 
+export type FeedbackType = keyof typeof feedbackTypes
+
+
 export function WidgetForm() {
+
+    const [feedbackType, setFeedbackType] = useState< FeedbackType | null>(null);
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-                <CloseButton/>
-            </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key,value]) => {
-                        return (
-                            <button>
-                                <img src={value.image.source} alt={value.image.alt} />
-                                <span>{value.title}</span>
-                            </button>
-                        )
-                    })}
-            </div>
+            {!feedbackType ? 
+            (
+                < FeedbackTypeStep onFeedbackChanged={setFeedbackType} />
+            ) : (
+                < FeedbackContentStep feedbackType={feedbackType} />
+            ) 
+            }
 
             <footer className="text-ts text-neutral-400">
              Feito com ♥ pela <a className="underline underline-offset-2" href="">Rocketseat</a>
