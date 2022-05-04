461e6c2a393ff1b7220e5bf3e3b84887f4c75486
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-05-03 21:35:36 -0300

    stage2

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from "../../assets/idea.svg"
 import otherImageUrl from "../../assets/other.svg"
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 
 export const feedbackTypes = {
@@ -38,18 +39,36 @@ export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
 
+    function handleRestartFeedback() {
+        setFeedbackSent(false)
+        setFeedbackType(null)
+    }
+
     const [feedbackType, setFeedbackType] = useState< FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false)
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            {!feedbackType ? 
-            (
-                < FeedbackTypeStep onFeedbackChanged={setFeedbackType} />
+            {feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
             ) : (
-                < FeedbackContentStep feedbackType={feedbackType} />
+               <>
+                    {!feedbackType ? 
+                    (
+                        < FeedbackTypeStep onFeedbackChanged={setFeedbackType} />
+                    ) : (
+                        < FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                    onFeedbackSent={() => setFeedbackSent(true)}
+                    />
             ) 
             }
+
+
+               </> 
+            )}
 
             <footer className="text-ts text-neutral-400">
              Feito com ♥ pela <a className="underline underline-offset-2" href="">Rocketseat</a>
